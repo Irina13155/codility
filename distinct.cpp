5eb2bf043e275f2911316ba3abcff488a5493988
Irina Pashchenko <ira.pashchenko15@gmail.com>
2025-02-28 17:12:35 +0100

    changed names

--- distinct.cpp
+++ distinct.cpp
@@ -1,14 +1,17 @@
+// https://app.codility.com/programmers/lessons/6-sorting/distinct/
+// 
+// Task Score: 100%
+// Correctness: 100%
+// Performance: 100%
+// Detected time complexity: O(N*log(N))
+//
+
 #include <vector>
 #include <iostream>
 #include <set>
 
 int solution(std::vector<int> &A) {
     std::set<int> set(begin(A), end(A));
-
     return set.size();
-}
-
-int main() {
-    return 0;
 }
  
